506854a5b1b3c2ecd20543975c797ea22df2e8f5
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-04-19 13:56:51 +0200

    path a request param

--- src/main/java/sk/stuba/uim/fei/oop/HelloController.java
+++ src/main/java/sk/stuba/uim/fei/oop/HelloController.java
@@ -1,6 +1,8 @@
 package sk.stuba.uim.fei.oop;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +17,16 @@ public class HelloController {
     @GetMapping("/helloCount")
     public HelloResponse helloCount() {
         return new HelloResponse(this.counter++, "hello world");
+    }
+
+    @GetMapping("/path/{name}")
+    public HelloResponse helloPath(@PathVariable(name = "name") String name) {
+        return new HelloResponse(this.counter++, "hello " + name);
+    }
+
+    @GetMapping("/param")
+    public HelloResponse helloParam(@RequestParam(name = "name", defaultValue = "world") String name) {
+        return new HelloResponse(this.counter++, "hello " + name);
     }
 }
 
